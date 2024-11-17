b41249898e773affa48d34d29c11193d7a30a096
Yevgine <ev.shahinyan@gmail.com>
2024-11-18 02:49:00 +0400

    Added react-toolkit

--- src/pages/Users/Users.jsx
+++ src/pages/Users/Users.jsx
@@ -4,13 +4,20 @@ import { useDispatch, useSelector } from 'react-redux'
 import { getUsersThunk } from '../../store/userReducer';
 import User from '../../components/User/User';
 import Pagination from '../../components/Pagination/pagination';
+import { API } from '../../api/api';
 
 function Users() {
     const dispatch = useDispatch();
     const { users, page, usersPerPage, totalUsers } = useSelector((state) => state.usersState)
+    const aaa = useSelector((state) => state)
+    console.log(aaa, 'aaa');
 
     useEffect(() => {
         dispatch(getUsersThunk(page, usersPerPage))
+        // API.getUsers(page, count).then((res) => {
+        //     dispatch(getUsersAC(res.data.items));
+        //     dispatch(setTotaalUsersCountAC(res.data.totalCount));
+        // });
     }, [page])
 
     return (
